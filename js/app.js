523228b2be0be8b448e339a345ce3f87bd17c40f
Guilherme Pola <guilhermepola03@gmail.com>
2022-08-06 15:53:55 -0300

    bug fixes

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
+
+var body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+});
+
+function login(params) {
+    window.location.href = `${pagename}/assets/pages/home.html`;
+}
